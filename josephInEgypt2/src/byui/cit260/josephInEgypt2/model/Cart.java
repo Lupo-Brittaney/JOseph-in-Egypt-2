c42c72be562efe2273e9ecb4795fedb1f2057ccb
mcarey10 <matttcarey@gmail.com>
2015-10-19 19:42:58 -0600

    Cart and Character

--- josephInEgypt2/src/byui/cit260/josephInEgypt2/model/Cart.java
+++ josephInEgypt2/src/byui/cit260/josephInEgypt2/model/Cart.java
@@ -1,6 +1,13 @@
-package JosephofEgypt;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.josephInEgypt2.model;
 
-public class Cart {
+import java.io.Serializable;
+
+public class Cart implements Serializable{
 
 	private int maxCapacity;
 	private int amountLoaded;
