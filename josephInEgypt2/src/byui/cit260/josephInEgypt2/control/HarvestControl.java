7e4af418435661e87e950f9c14d53625ba27f413
Britt <Britt@192.168.1.100>
2015-10-23 13:32:11 -0600

    10/22/2015 -update

--- josephInEgypt2/src/byui/cit260/josephInEgypt2/control/HarvestControl.java
+++ josephInEgypt2/src/byui/cit260/josephInEgypt2/control/HarvestControl.java
@@ -11,4 +11,24 @@ package byui.cit260.josephInEgypt2.control;
  */
 public class HarvestControl {
     
+    //Brittaney's piece
+    public double harvestResource( double unitsDesired, double emptyBarrels, double resourceAvailable){
+
+	if (unitsDesired > resourceAvailable || unitsDesired <1){
+	return -1;
+        }
+	if (emptyBarrels <1){
+	return -1;
+        }       
+	double barrelsNeeded= unitsDesired /2;
+	double remainingBarrels=emptyBarrels-barrelsNeeded;
+        
+        return remainingBarrels;
+        
+        /* if statements for later use
+        if (remainingBarrels >=0)
+	RETURN “You may harvest”+ desiredUnits +”units.”
+	If (remainingSpaces <0)
+	RETURN “Not enough empty barrels available.”*/
+    }
 }
