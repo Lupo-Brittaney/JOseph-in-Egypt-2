e31a28bad30d75c5b0c74518239a11aa92993136
mcarey10 <matttcarey@gmail.com>
2015-10-26 23:52:16 -0600

    10/22/2015

--- josephInEgypt2/src/byui/cit260/josephInEgypt2/control/InventoryControl.java
+++ josephInEgypt2/src/byui/cit260/josephInEgypt2/control/InventoryControl.java
@@ -10,5 +10,18 @@ package byui.cit260.josephInEgypt2.control;
  * @author Matt PC
  */
 public class InventoryControl {
-    
-}
+
+    public double collectMaterial(double noUnits, double cartSpace, double woodAvailable) {
+
+	if (noUnits > woodAvailable || noUnits <1){
+	return -1;
+        }
+	if (cartSpace <1){
+        return -1;
+        }
+        double spaceNeeded= noUnits/ 2;
+	double remainingSpaces=cartSpace- spaceNeeded;
+	return remainingSpaces;
+        }
+    }
+
