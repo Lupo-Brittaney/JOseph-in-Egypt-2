62e40622b55200b5ec9e100495572c3785333b8a
Britt <Britt@192.168.1.100>
2015-11-14 12:46:49 -0700

    11/04 update

--- josephInEgypt2/src/byui/cit260/josephInEgypt2/view/UnloadMenuView.java
+++ josephInEgypt2/src/byui/cit260/josephInEgypt2/view/UnloadMenuView.java
@@ -5,14 +5,71 @@
  */
 package byui.cit260.josephInEgypt2.view;
 
+import byui.cit260.josephInEgypt2.control.GameControl;
+import byui.cit260.josephInEgypt2.model.Cart;
+import static java.time.Clock.system;
+import java.util.Scanner;
+import josephinegypt2.JosephInEgypt2;
+
 /**
  *
  * @author Britt
  */
-class UnloadMenuView {
-
-    void displayUnloadMenu() {
-        System.out.println("\n*** displayUnloadMenu stub function called ***");
+public class UnloadMenuView extends View {
+    public UnloadMenuView(){
+        super("\n"
+            + "\n----------------------------------------------"
+            + "\n| Unload Menu                                  |"
+            + "\n----------------------------------------------"
+            + "\nU -Unload Cart"
+            + "\nE - Exit"
+            + "\n----------------------------------------------");
+        
     }
+    
+
+    @Override
+    public boolean doAction(Object obj) {
+         String value = (String)obj;
+        
+        value = value.toUpperCase();
+        char choice = value.charAt(0);
+        switch (choice) {
+             case 'U': // unload cart
+                this.unloadCart();
+                break;
+            case 'E': // Exit game
+                return false;
+            default:
+                System.out.println("\n** Invalid selection ** Try again");
+                break;      
+        }
+        return true;
+        }
+
+    private void unloadCart() {
+        //obtain contents of the cart
+        this.cartContents();
+        //add items from cart to amount stored
+        this.addStored();
+        //empty contents- set cart to empty
+        this.emptyCart();
+        
+    }
+
+           
+
+    private void cartContents() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void emptyCart() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void addStored() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
