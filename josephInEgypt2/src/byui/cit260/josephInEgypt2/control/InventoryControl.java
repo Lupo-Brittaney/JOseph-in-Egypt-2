b31a15d23c286a1b7eda420777fb740fabe3180d
Britt <Britt@192.168.1.100>
2015-12-01 14:54:24 -0700

    Merge origin/master
    Conflicts:
    	josephInEgypt2/src/byui/cit260/josephInEgypt2/model/Cart.java
    	josephInEgypt2/src/byui/cit260/josephInEgypt2/model/Location.java
    	josephInEgypt2/src/byui/cit260/josephInEgypt2/model/Pyramid.java

--- josephInEgypt2/src/byui/cit260/josephInEgypt2/control/InventoryControl.java
+++ josephInEgypt2/src/byui/cit260/josephInEgypt2/control/InventoryControl.java
@@ -5,19 +5,21 @@
  */
 package byui.cit260.josephInEgypt2.control;
 
+import byui.cit260.josephInEgypt2.exceptions.InventoryControlException;
+
 /**
  *
  * @author Matt PC
  */
 public class InventoryControl {
 
-    public double collectMaterial(double noUnits, double cartSpace, double woodAvailable) {
-
+    public double collectMaterial(double noUnits, double cartSpace, double woodAvailable) 
+                throws InventoryControlException{
 	if (noUnits > woodAvailable || noUnits <1){
-	return -1;
+	throw new InventoryControlException("Invalid number entered");
         }
 	if (cartSpace <1){
-        return -1;
+        throw new InventoryControlException("Not enough space in your cart");
         }
         double spaceNeeded= noUnits/ 2;
 	double remainingSpaces=cartSpace- spaceNeeded;
